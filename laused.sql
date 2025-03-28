de7d43008683b2c6d5563f2b79e2b1509676dff2
Hussein Tahmazov <tahmazovhussejn@gmail.com>
2025-03-28 11:56:57 +0200

    kõik select'id

--- laused.sql
+++ laused.sql
@@ -179,7 +179,7 @@ Salary nvarchar(50),
 DepartmentId int
 )
 
---?
+-- sisestab Department andmed ja Emplooes
 insert into Department (Id, DepartmentName, Location, DepartmentHead)
 values (1, 'IT', 'London', 'Rick')
 insert into Department (Id, DepartmentName, Location, DepartmentHead)
@@ -214,12 +214,12 @@ values (10, 'Russell', 'Male', 8800, NULL)
 
 select * from Employees
 
----?
+--- SELECT DISTINCT avaldust kasutatakse ainult erinevate (erinevate) väärtuste tagastamiseks.
 select distinct Name, DepartmentId from Employees
 
----?
+--- teeb kokkuvõtte
 select sum(cast(Salary as int)) from Employees
----?
+--- teeb minimaalse
 select min(cast(Salary as int)) from Employees
 
 
@@ -232,12 +232,16 @@ add DepartmentId
 int null
 
 
---?
+-- lisab uue andmetüübi nimega „MiddleName“
 alter table Employees
 add MiddleName nvarchar(30)
 
 alter table Employees
 add LastName nvarchar(30)
+
+-- sela pole "FirstName"
+alter table Employees
+add FirstName nvarchar(30)
 
 update Employees set FirstName = 'Tom', MiddleName = 'Nick', LastName = 'Jones'
 where Id = 1
